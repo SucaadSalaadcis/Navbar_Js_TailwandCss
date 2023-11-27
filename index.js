892ff235d220449ebcbd580d1276ba9afde4f2ce
sucaad salaad <SucaadSalaad520@gmail.com>
2023-11-27 09:50:51 -0800

    changes

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const iconButton = document.querySelector("#iconButton");
+const closeButton = document.querySelector("#closeButton");
 const links = document.querySelector("#links");
 
 
 iconButton.addEventListener("click", function () {
-    links.style.display = "flex"
+    links.style.display = "flex";
+    closeButton.style.display = "block";
+    iconButton.style.display = "none";
 })
+
+closeButton.addEventListener("click", function () {
+    links.style.display = "none";
+    iconButton.style.display = "block";
+    closeButton.style.display = "none";
+})
+
