93344c5a25e7ff1e69dcadeb0b8f60bc7988ce9f
sucaad salaad <SucaadSalaad520@gmail.com>
2023-11-27 09:00:37 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const iconButton = document.querySelector("#iconButton");
+const links = document.querySelector("#links");
+
+
+iconButton.addEventListener("click", function () {
+    links.style.display = "flex"
+})
